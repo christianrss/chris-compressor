32d61f2a32fc94886caecd5fecb0577a8a789ed9
Christian Rafael <christianparadix@gmail.com>
2026-02-04 00:01:50 -0300

    feat: Constructors

--- chris_compressor.c
+++ chris_compressor.c
@@ -1,6 +1,32 @@
 /* chris_compressor.c */
 #include <chris_compressor.h>
 
+amtlist *mkamtlist() {
+    int64 size;
+    amtlist *p;
+
+    size = (sizeof(struct s_amtlist)*Blocksize);
+    p = (amtlist *)alloc(size);
+    if (!p)
+        return (amtlist *)0;
+    zero($1 p, size);
+
+    p->length = 0;
+    p->capacity = Blocksize;
+
+    return p;
+}
+
+void zero(int8 *dst, int64 size) {
+    int8 *p;
+    int64 n;
+
+    for (p=dst, n=size; n; p++, n--)
+        *p = 0;
+
+    return;
+}
+
 int main() {
     return 0;
 }
